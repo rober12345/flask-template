cf727078a00df07e6482e58725aee6b79278835e
Roberto Ugalde <94400091+rober12345@users.noreply.github.com>
2024-12-23 10:03:05 -0600

    Update __init__.py
    to make it to MXN-USD predictor

--- app/routes/pages/__init__.py
+++ app/routes/pages/__init__.py
@@ -1,10 +1,27 @@
+"""
 # Flask modules
-from flask import Blueprint
+# from flask import Blueprint
 
 # Blueprint modules
-from .core import core_bp
+#from .core import core_bp
 
-pages_bp = Blueprint("pages", __name__, url_prefix="/")
+#pages_bp = Blueprint("pages", __name__, url_prefix="/")
 
 
-pages_bp.register_blueprint(core_bp)
+#pages_bp.register_blueprint(core_bp)
+"""
+
+from flask import Blueprint, render_template
+
+# Define the main blueprint for pages
+pages_bp = Blueprint("pages", __name__, template_folder='../../templates')
+
+# Define the home route directly here
+@pages_bp.route('/')
+def home():
+    return render_template('pages/home.html')
+
+
+
+
+
